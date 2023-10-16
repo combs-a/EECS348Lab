45330c103b611d2a34b4d0f8747b3e88c5f0ad4b
combs-a <combs.anya@gmail.com>
2023-10-15 20:27:35 -0500

    Finished first exercise.

--- Lab5_Combs/prog1.c
+++ Lab5_Combs/prog1.c
@@ -1,13 +1,37 @@
 #include <stdio.h>
 
-float average(float arr[], int start, int end) {
+float sum(float arr[], int start, int end) {
     float sum = 0;
-
+    
     for(int i = start; i < end; i++) {
         sum += arr[i];
     }
 
     return sum;
+}
+
+int findMin(float arr[]) {
+    int min = 0;
+    
+    for (int i = 0; i < 12; i++) {
+        if (arr[i] < arr[min]) {
+            min = i;
+        }
+    }
+    
+    return min;
+}
+
+int findMax(float arr[]) {
+    int max = 0;
+    
+    for (int i = 0; i < 12; i++) {
+        if (arr[i] > arr[max]) {
+            max = i;
+        }
+    }
+    
+    return max;
 }
 
 int main() {
@@ -25,13 +49,31 @@ int main() {
     }
 
     // Find total average.
-    float avg = average(input, 0, 12);
-
-    printf("\nSales summary: \n\nMinimum sales: %.2f\nMaximum sales: %.2f\nAverage Sales: %.2f\n", input[0], input[11], avg);
-
+    float avg = sum(input, 0, 12)/12;
+    int min = findMin(input);
+    int max = findMax(input);
+    
+    // Print minimum/maximum/average
+    printf("\nSales summary: \n\nMinimum sales: %.2f\t(%s)\nMaximum sales: %.2f\t(%s)\nAverage Sales: %.2f\n", input[min], months[min], input[max], months[max], avg);
+    
+    // 6-month moving average
     printf("\nSix-Month Moving Average Report:\n\n");
-
-    printf("\nSales Report (Highest to Lowest):\n\n");
+    
+    for (int i = 0; i <= 6; i++) {
+        float avg2 = sum(input, i, i+6)/6;
+        int width = 10;
+        
+        printf("%-*s - %10s\t%.2f\n", width, months[i], months[i+5], avg2);
+    }
+    
+    // Highest to lowest
+    printf("\nSales Report (Highest to Lowest):\n\n%10s\tSales\n", "Month");
+    int rem;
+    for (int i = 0; i < 12; i++) {
+        rem = findMax(input);
+        printf("%-*s\t%.2f\n", width, months[rem], input[rem]);
+        input[rem] = 0;
+    }
     
     return 0;
 }
